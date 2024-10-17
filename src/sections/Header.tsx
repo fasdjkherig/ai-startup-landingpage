84c6640a6708dc85fe3529dc396ecba9b7472296
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2024-10-17 10:55:02 +0300

    create logo ticker section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,8 +5,9 @@ import Button from "@/components/Button";
 export const Header = () => {
   return (
     <header className=" py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto md:backdrop-blur">
           <div className="h-12 w-12 border rounded-lg border-white/15 inline-flex justify-center items-center">
             <LogoIcon className="w-8 h-8 " />
           </div>
