a5d98961ac35de914e764fa8673df25c73f0c4de
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2024-10-31 07:02:27 +0300

    fix some symbols

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -83,7 +83,7 @@ export const Hero = () => {
           AI SEO
         </h1>
         <p className="text-center text-lg md:text-xl font-normal tracking-tighter text-white/70 mt-5 max-w-xl mx-auto">
-          Elevate your site's visibility effortlessly with AI, where smart
+          Elevate your site&apos;s visibility effortlessly with AI, where smart
           technology meets user-friendly SEO tools.
         </p>
         <div className="flex justify-center mt-5">
