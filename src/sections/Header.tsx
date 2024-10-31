ef014d8322533be3cefa7bb4c0bfa58881e9a055
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2024-10-31 05:32:04 +0300

    add menu for mobile view

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
 import Button from "@/components/Button";
 import Link from "next/link";
+import CloseIcon from "@/assets/icon-close.svg";
 
 export const Header = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
   return (
     <header className=" py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
       <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
@@ -43,9 +52,44 @@ export const Header = () => {
           <div className="flex justify-between gap-4 items-center">
             <Button>join waitlist</Button>
 
-            <MenuIcon className="w-8 h-8 md:hidden" />
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? (
+                <CloseIcon className="w-8 h-8" />
+              ) : (
+                <MenuIcon className="w-8 h-8 md:hidden" />
+              )}
+            </button>
           </div>
         </div>
+
+        {mobileDrawerOpen && (
+          <nav className="fixed right-0 mt-2 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <Link
+              href="#"
+              className="text-white/70 hover:text-white transition py-4"
+            >
+              Features
+            </Link>
+            <Link
+              href="#"
+              className="text-white/70 hover:text-white transition py-4"
+            >
+              Developers
+            </Link>
+            <Link
+              href="#"
+              className="text-white/70 hover:text-white transition py-4"
+            >
+              Pricing
+            </Link>
+            <Link
+              href="#"
+              className="text-white/70 hover:text-white transition py-4"
+            >
+              Changelog
+            </Link>
+          </nav>
+        )}
       </div>
     </header>
   );
